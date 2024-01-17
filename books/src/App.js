515371126a23cc9b9a3ada394fa740017f1b58e3
dipty31 <156056582+dipty31@users.noreply.github.com>
2024-01-17 11:35:18 +0530

    Add files via upload
    Made a book app. Added a downloaded css file for styling in it. Made components - BookCreate, BookList, BookShow, BookEdit. BookCreate and BookList are the child components of App component. and BookShow and BookEdit are the child components of BookList component. Implemented CRUD operations in the application. Used "axios" for sending and receiving http requests and responses, and used "json server" to make the application persistent.

---
+++ books/src/App.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
+
+function App(){
+    const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    //DON'T DO THIS:
+    //fetchBooks(); -> will cause a bug
+
+    const editBookById = async (id, newTitle ) => {
+        const response = await axios.put(`http://loacalhost:3001/books/${id}` , {
+            title: newTitle
+        });
+ 
+        const updatedBooks = books.map((book) => {
+            if(book.id === id){
+                return {...books, ...response.data};
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    }
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`)
+        const updatedBooks = books.filter((book)=>{
+            return book.id !==id;
+        });
+        setBooks(updatedBooks);
+    };
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books',{
+            title
+        }); 
+
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+        setBooks(updatedBooks);
+
+    };
+    return(
+        <div className="app">
+        <h1>Reading List</h1>
+            <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
+            <BookCreate onCreate={createBook} />
+        </div>
+    )
+
+}
+export default App;
