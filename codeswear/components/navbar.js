9da13b0409d300e4de77ca310f5c5bba73ee2d7b
dipty31 <deepti_choudhary@momentumventures.io>
2024-02-26 12:49:10 +0530

    added state and persistence in cart

--- codeswear/components/navbar.js
+++ codeswear/components/navbar.js
@@ -1,11 +1,27 @@
+import { IoIosCloseCircle } from "react-icons/io";
 import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
-import { CiShoppingCart } from "react-icons/ci";
+import { useRef } from "react";
+import { CiShoppingCart, CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import { BsFillBagCheckFill} from 'react-icons/bs';
 
-const Navbar = () => {
-  return (
-    <div className="flex flex-col md:flex-row md:justify-start justify-center item-center my-2 shadow-md">
+const Navbar = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
+    console.log(cart, addToCart, removeFromCart, clearCart, subTotal)
+    const toggleCart = () =>{
+        if(ref.current.classList.contains('translate-x-full')){
+            ref.current.classList.remove('translate-x-full');
+            ref.current.classList.add('translate-x-0');
+
+        } else if(!ref.current.classList.contains('translate-x-full')){
+            ref.current.classList.remove('translate-x-0');
+            ref.current.classList.add('translate-x-full');
+
+        }
+    }
+    const ref = useRef()
+;  return (
+    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md">
         <div className='logo mx-5'>
             <Link href='/'><Image src="/logo2.png" alt='codeswear' height={20} width={80} /></Link>
         </div>
@@ -17,9 +33,33 @@ const Navbar = () => {
                 <Link href='/mugs' ><li>Mugs</li></Link>
             </ul>
         </div>
-        <div className='cart absolute right-0 top-7 mx-5 font-bold'>
+        <div onClick={toggleCart} className='cart absolute right-0 top-7 mx-5 font-bold'>
             <CiShoppingCart className=' text-xl md:text-3xl font-bold'/>
         </div>
+
+
+    <div ref = {ref}className=' h-full w-72 sidecart absolute top-0 right-0 bg-blue-200 px-8 py-10 transform tranition-transform translate-x-full'>
+        <h2 className='font-bold text-xl text-center'>shoppong cart</h2>
+        <span onClick={toggleCart} className='absolute top-2 right-2 cursor-pointer text-xl text-indigo-500'><IoIosCloseCircle /></span>
+        <ol>
+        {Object.keys(cart)===0 && <div className='my-4 text-base font-normal'>No item in the cart!</div>}
+        {
+            Object.keys(cart).map((k)=>{
+            return(
+            <li key={k} className="text-center">
+            <div className="item flex my-5 rounded-lg">
+                <div className="w-2/3 font-semibold">{cart[k].name }</div> 
+                <div className="flex items-center justify-center w-1/3"><CiCircleMinus className="mx-3 cursor-pointer text-indigo-500" />{cart[k].qty}<CiCirclePlus className="mx-3 cursor-pointer text-purple-900"/></div> 
+            </div>
+            </li>);
+            })
+        }
+        </ol>
+        <div className="flex">
+        <button class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-sm"> <BsFillBagCheckFill className='m-1'/> Checkout </button>
+        <button onClick={clearCart} class="flex ml-0.5 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-sm">Clear Cart </button>
+        </div>
+    </div>
 
     </div>
   )
--- codeswear/pages/_app.js
+++ codeswear/pages/_app.js
@@ -1,11 +1,65 @@
+import React, {useState, useEffect} from 'react';
 import "@/styles/global.css";
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 
 export default function App({ Component, pageProps }) {
+  const [cart,setCart] = useState({});
+  const [subTotal, setSubTotal ] = useState(0);
+
+  useEffect ( () => {
+    console.log("hey I am a useEffect from _app.js");
+    try{
+      if(localStorage.getItem("cart")){
+        setCart(JSON.parse(localStorage.getItem("cart")))
+      }
+
+    } catch (error){
+        console.error(error);
+        localStorage.clear()
+    }
+  }, [])
+
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", JSON.stringify(myCart));
+    let subt = 0;
+    let keys = object.keys(cart);
+    for(let i=0; object.keys(cart).length; i++){
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty;   
+    }
+    setSubTotal(subt);
+  } 
+
+
+  const addToCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode] ={qty: 1, price, name, size, variant}
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  }
+const clearCart = () =>{
+  setCart({}); //this is just a request, there's no guarantee of it happening immediately. js will take care of it in it's own time and way.
+  saveCart({}); //hence giving empty object to saveCart, so that it doesn't save the old uupadated cart, in case of late updatation.
+}
+
+const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+  let newCart = cart;
+  if(itemCode in cart){
+    newCart[itemCode].qty = cart[itemCode].qty - qty;
+  } if(newCart[itemCode]["qty"] <= 0) {
+      delete newCart[itemCode];
+  }
+  setCart(newCart);
+  saveCart(newCart);
+}
+
   return( <>
-  <Navbar/>
-  <Component {...pageProps} />
+  <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} />
+  <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
   <Footer/>
   </>
   );
